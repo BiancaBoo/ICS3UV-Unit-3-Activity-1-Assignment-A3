4c21d659af4613cdb582c1f5ba8f26339b293f62
default <bianca.bertinato@stu.ocsb.ca>
2025-11-09 00:06:39 +0000

    added multiplication table

---
+++ main.ts
@@ -0,0 +1,27 @@
+/*
+* Author: Bianca Bertinato
+* Version: 1.0.0
+* Date: 2025-11-08
+* This program displays the multiplication table of 9 from 0 to 12 without using loops.
+*/
+
+// CONSTANT
+  const NUMBER: number = 9;
+
+  // PROCESS & OUTPUT
+console.log("MULTIPLICATION TABLE OF " + NUMBER + ":");
+console.log(NUMBER + " x 0 = " + (NUMBER * 0));
+console.log(NUMBER + " x 1 = " + (NUMBER * 1));
+console.log(NUMBER + " x 2 = " + (NUMBER * 2));
+console.log(NUMBER + " x 3 = " + (NUMBER * 3));
+console.log(NUMBER + " x 4 = " + (NUMBER * 4));
+console.log(NUMBER + " x 5 = " + (NUMBER * 5));
+console.log(NUMBER + " x 6 = " + (NUMBER * 6));
+console.log(NUMBER + " x 7 = " + (NUMBER * 7));
+console.log(NUMBER + " x 8 = " + (NUMBER * 8));
+console.log(NUMBER + " x 9 = " + (NUMBER * 9));
+console.log(NUMBER + " x 10 = " + (NUMBER * 10));
+console.log(NUMBER + " x 11 = " + (NUMBER * 11));
+console.log(NUMBER + " x 12 = " + (NUMBER * 12));
+
+console.log("\nDone.");
